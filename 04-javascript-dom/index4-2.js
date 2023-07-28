668f894135950820c96459254b52e811fc5c493f
Vamos-Hyuk <kyle910912@gmail.com>
2023-07-28 23:40:29 +0900

    study : js 실습 방명록 만들기

---
+++ 04-javascript-dom/index4-2.js
@@ -0,0 +1,26 @@
+let ind = 0;
+
+document.querySelector('#upBtn').addEventListener('click', () => {
+  let year = new Date().getFullYear();
+  let month = new Date().getMonth();
+  let day = new Date().getDay();
+  let hour = new Date().getHours();
+  let minute = new Date().getMinutes();
+
+  const newTr = document.createElement('tr');
+  const newIndex = document.createElement('td');
+  const newVisitor = document.createElement('td');
+  const newContent = document.createElement('td');
+  const newWriteDay = document.createElement('td');
+
+  newIndex.innerHTML = ind + 1;
+  newVisitor.innerHTML = document.querySelector('#visitor').value;
+  newContent.innerHTML = document.querySelector('#content').value;
+  newWriteDay.innerHTML = `${year}-${month}-${day}-${hour}:${minute}`;
+
+  newTr.append(newIndex, newVisitor, newContent, newWriteDay);
+
+  document.querySelector('table').append(newTr);
+  document.querySelector('#visitor').value = '';
+  document.querySelector('#content').value = '';
+});
