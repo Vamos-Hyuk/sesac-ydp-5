f4596553f0e0429c7cbc5d30d5aa57276bfd9a82
Vamos-Hyuk <kyle910912@gmail.com>
2023-07-28 16:40:41 +0900

    study : js documents

---
+++ 04-javascript-dom/index2.js
@@ -0,0 +1,45 @@
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.title);
+console.log(document.URL);
+// console.log(document.domain);
+
+// document 객체를 이용해 HTML 요소 선택
+// 1. getElementById
+console.log(document.getElementById('green'));
+console.log(document.getElementById('red'));
+
+// 2. getElementsByClassName
+console.log(document.getElementsByClassName('pink')); // 유사배열
+console.log(document.getElementsByClassName('pink')[1]);
+
+// 3. getElementsByTagName
+console.log(document.getElementsByTagName('div'));
+
+// 4. getElementsByName
+console.log(document.getElementsByName('id'));
+
+// 5. querySelector(css selector)
+// : 처음 발견한 하난만 가지고옴
+console.log(document.querySelector('.pink'));
+console.log(document.querySelector('.others'));
+console.log(document.querySelector('#green'));
+console.log(document.querySelector('div'));
+console.log(document.querySelector('[name="id"]'));
+
+// 6. querySelectorAll(css selector)
+// : 선택자에 해당되는 모든 요소를 선택
+console.log(document.querySelectorAll('.pink'));
+console.log(document.querySelectorAll('.others'));
+console.log(document.querySelectorAll('#green'));
+console.log(document.querySelectorAll('div'));
+console.log(document.querySelectorAll('[name="id"]'));
+
+console.log(document.querySelectorAll('.pink')[0]);
+console.log(document.querySelectorAll('.pink')[1]);
+console.log(document.querySelectorAll('.pink')[2]);
+console.log(document.querySelectorAll('.pink')[3]);
+
+console.log(document.getElementsByTagName('input'));
+console.log(document.getElementsByName('id'));
