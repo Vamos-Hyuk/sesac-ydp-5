dcd4d3a42fa9a2efb5ae7d053c14ce8bab1aa1ee
Vamos-Hyuk <kyle910912@gmail.com>
2023-08-09 15:13:13 +0900

    study: module

--- 08-node/math2.js
+++ 08-node/math2.js
@@ -8,18 +8,34 @@ const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
 
-// 객체로 감싸서 내보내기
-module.exports = {
-  add: add,
-  PI: PI,
-  E: E,
-  sub: sub,
-  mul: mul,
-  div: div,
-};
+// case1. 객체로 감싸서 내보내기
+// module.exports = {
+//   add: add,
+//   PI: PI,
+//   E: E,
+//   sub: sub,
+//   mul: mul,
+//   div: div,
+// };
 
 // module.exports = {
 //     add,   //obj에쇼서 key, value가 동일할 때 생략 가능
 //     PI,
 //     E,
 // }
+
+// // case2. 하나씩 내보내기
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.E = E;
+// module.exports.sub = sub;
+// module.exports.mul = mul;
+// module.exports.div = div;
+
+// case2 를 더 간단히
+exports.add = add;
+exports.PI = PI;
+exports.E = E;
+exports.sub = sub;
+exports.mul = mul;
+exports.div = div;
