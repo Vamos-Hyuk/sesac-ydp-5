304583aae61894296e978a79ab9ba0a55e2329de
Vamos-Hyuk <kyle910912@gmail.com>
2023-08-14 15:33:19 +0900

    Promise 공부

---
+++ 09-js-upgrade/promise.js
@@ -0,0 +1,13 @@
+// Promise
+// 비동기 함수를 동기 처리하기 위해 만들어진 객체
+// 성공과 실패를 분리하여 반환
+// 비동기 작업이 완료된 이후에 다음 작업을 연결시켜 진행할 수 있는 기능을 가짐
+// ES6에서 추가된 JS문법
+// new Promise 로 만들어서 사용!
+// executor 매개변수 (resolve  와 reject 모두 callback)
+
+// Promise의 상태 * 면접 단골 질문 *
+// Pending(대기) :
+// Fulfilled(이행) :
+// Rejected(거부) :
+// Settled :
