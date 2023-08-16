a94c23b4d9fe0f2638b03f089acdc69a6de25765
Vamos-Hyuk <kyle910912@gmail.com>
2023-08-16 17:44:31 +0900

    form 추가

--- 11-form-practice/app.js
+++ 11-form-practice/app.js
@@ -12,6 +12,32 @@ app.set('/views', 'views');
 app.use(express.urlencoded({ extended: true })); // post 요청으로 들어오는 모든 형식의 데이터를 파싱
 app.use(express.json()); // json 형식으로 데이터를 주고 받음
 
+// 라우팅(routing) : 주소를 설정하는 행위
+//  - [요청방식(get, post, ...) + 요청 경로] 세트가 겹치면 안돼!
+// 라우트(route) : 주소
+
+// get / 경로: 요청시 main 페이지 보여주기
+app.get('/', (req, res) => {
+  res.render('main');
+});
+
+app.get('/practice1', (req, res) => {
+  res.render('practice1');
+});
+
+app.get('/practice2', (req, res) => {
+  res.render('practice2');
+});
+
+app.get('/result1', (req, res) => {
+  console.log(req.query);
+  res.render('result');
+});
+
+app.post('/result2', (req, res) => {
+  res.render('result');
+});
+
 app.listen(PORT, () => {
   console.log(`${PORT} is opening!`);
 });
