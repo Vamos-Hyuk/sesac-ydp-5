01141254dc360a36218445c690efa4f047ea2435
Vamos-Hyuk <kyle910912@gmail.com>
2023-08-24 23:42:49 +0900

    study: MySQL 실습

--- 14-mysql/DML.sql
+++ 14-mysql/DML.sql
@@ -192,7 +192,58 @@ select * from customer where addr like '미국%' or addr like '영국%';
 -- 휴대폰 번호 마지막 자리가 4가 아닌 고객 검색
 select * from customer where phone not like '%_4';
 
+create table member ( 
+  id    varchar(20) primary key not null,
+  name  varchar(5) not null,
+  age	int(10), 
+  gender  varchar(2) not null, 
+  email    varchar(50),
+  promotion varchar(2) default 'x'
+);
+desc member;
+
+drop table member;
+
+alter table member add interest varchar(100);
+alter table member drop column age;
+alter table member modify id varchar(10);
 
 
+create table user ( 
+  id  varchar(10) primary key not null,
+  pw  varchar(20) not null,
+  name	varchar(5) not null, 
+  gender enum('F', 'M', '' ) default '', 
+  birthday  date not null,
+  age int(3) not null default 0
+);
+desc user;
 
+insert into user values
+	 ('hong1234', '8o4bkg', '홍길동', 'M', '1990-01-31', '33'),
+     ('sexysung', '87awjkdf', '성춘향', 'F', '1992-03-31', '31'),
+     ('power70', 'qxur8sda', '변사또', 'M', '1970-05-02', '53'),
+     ('hanjo', 'jk48fn4', '한조', 'M', '1984-10-18', '39'),
+     ('widowmaker', '38ewifh3', '위도우', 'F', '1976-06-27', '47'),
+     ('dvadva', 'k3f3ah', '송하나', 'F', '2001-06-03', '22'),
+     ('jungkrat', '4ifha7f', '정크랫', 'M', '1999-11-11', '24');
+     
+select * from user;
 
+select * from user order by birthday asc;
+
+select * from user where gender = 'M' order by name desc;
+
+select id, name from user where year(birthday) between 1990 and 1999;
+
+select * from user where month(birthday) = 6 order by birthday asc;
+
+select * from user where gender = 'M' and year(birthday) between 1970 and 1979;
+
+select * from user order by age desc limit 3;
+
+select * from user where age >= 25 and age <= 50;
+
+update user set pw = '12345678' where id = 'hong1234';
+
+delete from user where id = 'jungkrat';
