7650e57c9fffdc9da846c00ed342a9d94b840114
Vamos-Hyuk <kyle910912@gmail.com>
2023-09-01 13:37:43 +0900

    study: views 설정

--- 12-2-mvc_dynamic_form/app.js
+++ 12-2-mvc_dynamic_form/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
-app.use('/views', '/views');
+app.set('/views', '/views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
