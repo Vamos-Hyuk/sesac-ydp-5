f83228eaf522b16fe6d61464a650d9236bfe4664
Vamos-Hyuk <kyle910912@gmail.com>
2023-10-04 14:05:08 +0900

    study: react실습

--- allPractice/allpractice-app/src/App.js
+++ allPractice/allpractice-app/src/App.js
@@ -1,24 +1,32 @@
+import { useState } from 'react';
 import Input from './Input';
 import Result from './Result';
 import Select from './Select';
 
 function App() {
+  // 상태
+  const [data, setData] = useState({
+    fruit: 'apple',
+    background: 'black',
+    color: 'white',
+    content: 'text',
+  });
   return (
     <>
       <div
         style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}
       >
-        <Select />
+        <Select setData={setData} />
       </div>
       <div
         style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}
       >
-        <Input />
+        <Input setData={setData} />
       </div>
       <div
         style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}
       >
-        <Result />
+        <Result data={data} />
       </div>
     </>
   );
--- allPractice/allpractice-app/src/Input.js
+++ allPractice/allpractice-app/src/Input.js
@@ -1,7 +1,18 @@
-function Input() {
+function Input(props) {
+  const setData = props.setData;
+
+  const handleInput = (e) => {
+    const content = e.target.value;
+    // console.log(content); // input에 입력한 값
+
+    setData((data) => {
+      // console.log(data);
+      return { ...data, content: content };
+    });
+  };
   return (
     <>
-      내용 :{" "}
+      내용 :{' '}
       <input
         type="text"
         onChange={(e) => {}}
