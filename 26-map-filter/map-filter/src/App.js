5ff028e5b2f61dec3e433724e2bd919bb7e13e3a
Vamos-Hyuk <kyle910912@gmail.com>
2023-10-05 14:52:05 +0900

    study: ref

--- 26-map-filter/map-filter/src/App.js
+++ 26-map-filter/map-filter/src/App.js
@@ -1,9 +1,12 @@
 import Alphabet from './Alphabet';
+import EventEx from './ex/EventEx';
 
 function App() {
   return (
     <div className="App">
       <Alphabet />
+      <hr />
+      <EventEx />
     </div>
   );
 }
